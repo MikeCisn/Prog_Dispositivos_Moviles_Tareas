f190e82d6329f02e29df41a9606ebe5010926671
Miguel <123128620+MikeCisn@users.noreply.github.com>
2023-10-03 20:40:06 -0600

    Create Practica2.kt

---
+++ Practica2.kt
@@ -0,0 +1,11 @@
+//Cisneros Gonzalez Miguel
+//5CV50
+const val pi=3.1416f
+fun main() {
+    //Variable decimal
+   val numero=5.545f
+    //Calculo de Perimetro del circulo
+   val perimetro =2 * pi * numero
+    println("El perimetro del circulo es: $perimetro")
+       
+}
